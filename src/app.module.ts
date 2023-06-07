9feedf61c618972cf8359159adf8e0e9af4e9e85
Marcelo Henrique <73535877+feitan02@users.noreply.github.com>
2023-06-07 11:14:06 -0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://0.0.0.0/nest'), UsersModule, AuthModule, ProductsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
